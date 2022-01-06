c8a8c3b49c4b49de643d71a0de9539c7b3f41ef5
rajes <rajes@192.168.0.4>
2022-01-06 21:38:43 +0530

    Initial commit

---
+++ src/Person.java
@@ -0,0 +1,18 @@
+
+public class Person {
+  
+    int id=18;
+    double salary=30000;
+    String gender="male";
+    String name="sai";
+    public static void main(String[] args) {
+  
+   	 Person person=new Person();
+ 
+   	 System.out.println(person.id);
+   	 System.out.println(person.name);
+   	 System.out.println(person.gender);
+   	 
+    }
+
+}
